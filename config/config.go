081d440e960eaada6a3b4e99fca5122f863920ff
d.sukhodolsky <sdimon13@gmail.com>
2024-11-17 20:12:32 +0300

    Add reply channel messages for users

--- config/config.go
+++ config/config.go
@@ -4,14 +4,17 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	AppID       int32
-	AppHash     string
-	PhoneNumber string
+	AppID         int32
+	AppHash       string
+	PhoneNumber   string
+	ChannelIDs    []int64
+	TargetUserIDs []int64
 }
 
 func LoadConfig() *Config {
@@ -23,20 +26,39 @@ func LoadConfig() *Config {
 	appIDStr := os.Getenv("APP_ID")
 	appHash := os.Getenv("APP_HASH")
 	phoneNumber := os.Getenv("PHONE_NUMBER")
+	channelIDsStr := os.Getenv("CHANNEL_IDS")
+	targetUserIDsStr := os.Getenv("TARGET_USER_IDS")
 
-	if appIDStr == "" || appHash == "" || phoneNumber == "" {
-		log.Fatal("Не удалось получить APP_ID, APP_HASH или PHONE_NUMBER из переменных окружения")
+	if appIDStr == "" || appHash == "" || phoneNumber == "" || channelIDsStr == "" || targetUserIDsStr == "" {
+		log.Fatal("Не удалось получить конфигурационные параметры из .env")
 	}
 
-	// Преобразуем APP_ID из строки в int32
 	appID, err := strconv.Atoi(appIDStr)
 	if err != nil {
 		log.Fatalf("Ошибка преобразования APP_ID в число: %v", err)
 	}
 
+	// Преобразуем списки ID каналов и пользователей в массивы
+	channelIDs := parseIDs(channelIDsStr)
+	targetUserIDs := parseIDs(targetUserIDsStr)
+
 	return &Config{
-		AppID:       int32(appID),
-		AppHash:     appHash,
-		PhoneNumber: phoneNumber,
+		AppID:         int32(appID),
+		AppHash:       appHash,
+		PhoneNumber:   phoneNumber,
+		ChannelIDs:    channelIDs,
+		TargetUserIDs: targetUserIDs,
 	}
+}
+
+// parseIDs преобразует строку "1,2,3" в []int64
+func parseIDs(idsStr string) []int64 {
+	ids := []int64{}
+	for _, id := range strings.Split(idsStr, ",") {
+		parsedID, err := strconv.ParseInt(strings.TrimSpace(id), 10, 64)
+		if err == nil {
+			ids = append(ids, parsedID)
+		}
+	}
+	return ids
 }
--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"github.com/amarnathcjd/gogram/telegram"
 	"log"
 	"post-wizard/config"
+
+	"github.com/amarnathcjd/gogram/telegram"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 
 	client, err := telegram.NewClient(telegram.ClientConfig{
-		AppID:   cfg.AppID,
-		AppHash: cfg.AppHash,
+		AppID:    cfg.AppID,
+		AppHash:  cfg.AppHash,
+		LogLevel: telegram.LogInfo,
 	})
 
 	if err != nil {
@@ -22,11 +24,35 @@ func main() {
 
 	client.Login(cfg.PhoneNumber)
 
+	log.Println(cfg.ChannelIDs)
+
+	// Подписываемся на события сообщений
 	client.On(telegram.OnMessage, func(message *telegram.NewMessage) error {
 		log.Printf("Received message from id: %d, name: %s: %s\n", message.Sender.ID, message.Sender.Username, message.Message.Message)
-		message.Reply("Hello from Gogram!")
+
+		// Игнорируем сообщения, не пришедшие из каналов
+		if message.Channel == nil {
+			return nil
+		}
+
+		for _, channelID := range cfg.ChannelIDs {
+			if message.Channel.ID == channelID {
+				// Пересылаем сообщение всем указанным пользователям
+				for _, userID := range cfg.TargetUserIDs {
+					_, err := client.SendMessage(userID, message.Message.Message)
+
+					if err != nil {
+						log.Printf("Ошибка при отправке сообщения пользователю %d: %v", userID, err)
+					} else {
+						log.Printf("Сообщение переслано пользователю %d", userID)
+					}
+				}
+				break // Не нужно повторять цикл для других каналов
+			}
+		}
+
 		return nil
-	}, telegram.FilterPrivate)
+	})
 
 	client.Idle()
 }
