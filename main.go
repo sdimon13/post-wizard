c38092db91a07fdec4f48adb0256ff026229221c
d.sukhodolsky <sdimon13@gmail.com>
2024-11-20 19:12:32 +0300

    update conditions

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/amarnathcjd/gogram/telegram"
 	"log"
 	"post-wizard/config"
@@ -65,17 +64,6 @@ func containsLinksOrHashtags(text string) bool {
 	return linkRegex.MatchString(text) || hashtagRegex.MatchString(text)
 }
 
-func forwardText(client *telegram.Client, message *telegram.NewMessage, targetUserIDs []int64) {
-	for _, userID := range targetUserIDs {
-		_, err := client.SendMessage(userID, message.Message.Message)
-		if err != nil {
-			log.Printf("Ошибка при отправке текстового сообщения пользователю %d: %v", userID, err)
-		} else {
-			log.Printf("Текстовое сообщение успешно переслано пользователю %d", userID)
-		}
-	}
-}
-
 func forwardMedia(client *telegram.Client, message *telegram.NewMessage, targetUserIDs []int64) {
 	for _, userID := range targetUserIDs {
 		_, err := client.Forward(userID, message.Channel.ID, []int32{message.ID}, &telegram.ForwardOptions{
@@ -87,33 +75,4 @@ func forwardMedia(client *telegram.Client, message *telegram.NewMessage, targetU
 			log.Printf("Медиа-сообщение успешно переслано пользователю %d", userID)
 		}
 	}
-}
-
-func forwardMessage(client *telegram.Client, message *telegram.NewMessage, targetUserIDs []int64) {
-	for _, userID := range targetUserIDs {
-		_, err := client.SendMessage(userID, &telegram.NewMessage{
-			Message: message.Message,
-		})
-		if err != nil {
-			log.Printf("Ошибка при отправке текстового сообщения пользователю %d: %v", userID, err)
-		} else {
-			log.Printf("Текстовое сообщение успешно переслано пользователю %d", userID)
-		}
-	}
-}
-
-func saveMedia(client *telegram.Client, message *telegram.NewMessage, targetUserIDs []int64) {
-	options := &telegram.DownloadOptions{
-		Threads: 4,
-		ProgressCallback: func(totalBytes int64, downloadedBytes int64) {
-			fmt.Printf("Загружено %d из %d байт\n", downloadedBytes, totalBytes)
-		},
-	}
-	// Загрузка медиа-контента напрямую на диск
-	savedFilePath, err := message.Download(options)
-	if err != nil {
-		fmt.Println("Ошибка загрузки медиа:", err)
-		return
-	}
-	fmt.Println("Медиа успешно загружено в:", savedFilePath)
 }
